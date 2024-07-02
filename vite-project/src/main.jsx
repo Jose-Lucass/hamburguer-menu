1ee4f3de5db0b58c7fee622fa4af79b8c277940d
José lucas Soares <joselucas3005@gmail.com>
2024-07-02 18:12:38 -0300

    template project semi-complete

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './pages/header'
+import Main from './pages/main'
+import History from './pages/section-history'
+
+import Busca from './pages/location'
+import PapoFooter from './pages/footer'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
+    <Main/>
+    <History/>
+    <Busca/>
+    <PapoFooter/>
   </React.StrictMode>,
 )
