8fbc1f719875a630089f164eeca105b352ea75be
José lucas Soares <joselucas3005@gmail.com>
2024-07-02 18:12:05 -0300

    feat: Created the page show about papos hamburguer's combos

---
+++ vite-project/src/pages/main/index.jsx
@@ -0,0 +1,8 @@
+import Combo from "../../assets/combo-todo-dia.png"
+import { CombosImage } from "./styles"
+
+export default function Main(){
+    return(
+        <CombosImage src={Combo} alt="" />
+    )
+}
---
+++ vite-project/src/pages/main/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const CombosImage = styled.img`
+    width: 100%;
+    height: auto;
+`
